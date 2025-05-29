999dbe7fc53827b04592fba113cdb77025baa631
CuriousWonder1 <curiouswonders1234@gmail.com>
2025-05-30 09:36:14 +1000

    Update main.py

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ async def end(interaction: discord.Interaction):
 
     embed.add_field(
         name="\u200b",
-        value="Keep an eye out for future events in here or ⁠https://discord.com/channels/457619956687831050/1349087527557922988! 👀",
+        value="/n/ Keep an eye out for future events in here or ⁠https://discord.com/channels/457619956687831050/1349087527557922988! 👀",
         inline=False
     )
 
