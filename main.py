f29324cd92daaccf62e7e9523ecc8b9a178e1565
CuriousWonder1 <curiouswonders1234@gmail.com>
2025-05-30 11:57:32 +1000

    Update main.py

--- main.py
+++ main.py
@@ -305,15 +305,9 @@ async def end(interaction: discord.Interaction):
     for e in upcoming:
         embed.add_field(
             name=e["name"],
-            value=f"Starts <t:{int(e['start_time'].timestamp())}:F>\nCreated by: <@{e['creator']['id']}>",
+            value=f"Starts <t:{int(e['start_time'].timestamp())}:F>\nCreated by: <@{e['creator']['id']}>\nKeep an eye out for future events in here or https://discord.com/channels/457619956687831050/1349087527557922988! 👀",
             inline=False
         )
-
-    embed.add_field(
-        name="Part",  # Zero-width space for an empty name
-        value=f"Keep an eye out for future events in here or https://discord.com/channels/457619956687831050/1349087527557922988! 👀",
-        inline=False
-    )
 
     try:
         await interaction.channel.send(embed=embed)
